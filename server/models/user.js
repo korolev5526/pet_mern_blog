5ee8237cc763838838729f171c2ac93de08bd653
Nikita Korneev <nikita@gologinapp.com>
2023-04-25 15:15:04 +0700

    Create user schema

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserShema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+        }
+    ]
+    },
+    {timestamps: true},
+)
+
+export default mongoose.model('User', UserShema);
