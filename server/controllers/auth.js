67231759e04e91472d602d48c37a306511fa5ba2
Nikita Korneev <nikita@gologinapp.com>
2023-04-26 11:34:14 +0700

    create login request processing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 //Register user
 export const register = async (req, res) => {
@@ -38,9 +39,41 @@ export const register = async (req, res) => {
 //Login user
 export const login = async (req, res) => {
     try{
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        
+        if(!user) {
+            return res.json({
+                message: "Такого user не существует"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect) {
+            res.json({
+                message: 'Неверный пароль'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {expiresIn: '30d'}
+        )
+
+        res.json({
+            token, 
+            user, 
+            message: "Вы вошли в систему"
+        })
 
     } catch(error) {
-
+        res.json({
+            message: `Ошибка при авторизации. ${error}`
+        })
     }
 }
 //Get me
