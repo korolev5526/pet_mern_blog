4ddd3a0ae04962b42bc3b787218a7d47942c3928
Nikita Korneev <nikita@gologinapp.com>
2023-04-27 11:41:20 +0700

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
+  const isAuth = true;
+
+  const activeStyles = {
+    color: 'white',
+  }
+
   return (
-    <div>Navbar</div>
+    <div className='flex py-4 justify-between items-center'>
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">E</span>
+      {
+        isAuth && (
+          <ul className="flex gap-8">
+            <li>
+              <NavLink 
+                to='/' 
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyles : undefined}
+              >
+                Главная
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/posts' 
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyles : undefined}
+              >
+                Мои посты
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/new' 
+                className='text-xs text-gray-400 hover:text-white'
+                style={({ isActive }) => isActive ? activeStyles : undefined}
+              >
+                Добавить пост
+              </NavLink>
+            </li>
+          </ul>
+        )
+      }
+      <div className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2'>
+        { isAuth ? (
+        <button>Выйти</button>
+        ) : (
+          <Link to={'/login'}>Войти</Link>
+        )}
+        
+      </div>
+    </div>
   )
 }
